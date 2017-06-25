a3c7ecd81086a8be2d2b1c172356749d6d76f68f
tangchaoren3597 <15565203597@163.com>
2017-06-25 14:39:17 +0800

    添加输入input

---
+++ my-project/src/components/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="text" class="form-control"  placeholder="请输入姓名" v-model="name">
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="text" class="form-control" placeholder="请输入地址">
+      </div>
+      <button class="btn btn-default">提交</button>
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        name: 'hello world'
+      }
+    }
+  }
+</script>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import 'jquery'
+import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './components/home'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -10,6 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: '<Home/>',
+  components: { Home }
 })
